bb0b8ee971bde4e6428795e2dc13a537924ad23f
Mehad Hossen <mehad004938@gmail.com>
2025-11-25 09:38:35 +0600

    create payment related api

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ const port = process.env.PORT || 3000;
 // here setup the database url
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+// Stripe url
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.g6tkuix.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -38,14 +42,14 @@ async function run() {
       try {
         // console.log(req.query)
         const query = {};
-        const options={sort:{createdAt:-1}}
+        const options = { sort: { createdAt: -1 } };
 
         const { email } = req.query;
         if (email) {
           query.senderEmail = email;
         }
 
-        const cursor = parcelCollection.find(query,options);
+        const cursor = parcelCollection.find(query, options);
         const result = await cursor.toArray();
         res.send(result);
       } catch (error) {
@@ -77,24 +81,76 @@ async function run() {
       }
     });
 
-
-    // Delete api is here 
-     app.delete("/parcels/:id",async(req,res)=>{
-      try{
-        const id=req.params.id;
-         const query={_id:new ObjectId(id)}
-         const result=await parcelCollection.deleteOne(query);
-         res.send(result)
-
-      }
-      catch(error){
+    // Delete api is here
+    app.delete("/parcels/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await parcelCollection.deleteOne(query);
+        res.send(result);
+      } catch (error) {
         res.status(500).send({
-          success:false,
-          message:"Internal server error"
-
-        })
+          success: false,
+          message: "Internal server error",
+        });
       }
-     })
+    });
+
+    //Find one parcels to payment
+    app.get("/parcels/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await parcelCollection.findOne(query);
+
+        //  if not found
+        if (!result) {
+          return res.status(404).send({
+            success: false,
+            message: "Parcel not found",
+          });
+        }
+
+        // success
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({
+          success: false,
+          message: "Internal server error",
+        });
+      }
+    });
+
+    // Payment related api
+
+    app.post("/create-checkout-session", async (req, res) => {
+      const paymentInfo = req.body;
+      const amount = parseInt(paymentInfo.cost) * 100;
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            // Provide the exact Price ID (for example, price_1234) of the product you want to sell
+            price_data: {
+              currency: "USD",
+              unit_amount: amount,
+              product_data: {
+                name: paymentInfo.parcelName,
+              },
+            },
+            quantity: 1,
+          },
+        ],
+        customer_email: paymentInfo.senderEmail,
+        mode: "payment",
+        meta_data: {
+          parcelId: paymentInfo.parcelId,
+        },
+        success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success`,
+        cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-canceled`,
+      });
+
+      // res.redirect(303, session.url);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
